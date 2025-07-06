8331ec06c43153e281dc113725438f670e5e54ad
Shatha Almamari <shatha.almamari99@gmail.com>
2025-07-06 16:49:31 +0400

    added loop code

--- loop.java
+++ loop.java
@@ -1,14 +1,25 @@
 public class loop {
     public static void main(String[] args) {
         String[] names = {"Nasser", "Haitham", "Manal", "Hanin"};
-        System.out.print("Output: {");
+       
 		
 		
+		System.out.print("{");
+        for (int i = 0; i < names.length; i++) {
+            System.out.print("\"" + names[i] + "\"");
+            if (i < names.length - 1) System.out.print(", ");
+        }
+        System.out.println("}");
+		
+		
+		System.out.println();
+
+
         for (int i = 0; i < names.length; i++) {
             String reversed = new StringBuilder(names[i]).reverse().toString();
             System.out.print("\"" + reversed + "\"");
 			
-            if (i < names.length - 1) System.out.print(", ");
+            if (i < names.length - 1) System.out.print(",");
         }
         System.out.println("}");
     }
